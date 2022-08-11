35f38e873fd7ea8e49812639adc4195e0e4f26c3
Zack Philipps <hello@zackphilipps.com>
2022-08-11 11:58:16 -0400

    move mdsvex plugins to mdsvex config

--- mdsvex.config.js
+++ mdsvex.config.js
@@ -1,5 +1,8 @@
 import { defineMDSveXConfig as defineConfig } from 'mdsvex'
 // import { join } from 'path';
+import rehypeSlug from 'rehype-slug'
+import rehypeAutolinkHeadings from 'rehype-autolink-headings'
+import toc from 'remark-toc'
 
 const config = defineConfig({
   extensions: ['.svelte.md', '.md', '.svx'],
@@ -10,8 +13,8 @@ const config = defineConfig({
   //   dashes: 'oldschool'
   // },
 
-  remarkPlugins: [],
-  rehypePlugins: []
+  remarkPlugins: [toc],
+  rehypePlugins: [rehypeSlug, [rehypeAutolinkHeadings, { test: ['h2', 'h3', 'h4', 'h5', 'h6'] }]]
 })
 
 export default config
