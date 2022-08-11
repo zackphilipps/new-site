f854cfe539e03522b8a8fb76171f59db60b214a0
Zack Philipps <hello@zackphilipps.com>
2022-08-11 11:58:31 -0400

    fix og:image tag not populating

--- src/lib/components/PostLayout.svelte
+++ src/lib/components/PostLayout.svelte
@@ -4,7 +4,9 @@
   export let title
   export let slug
   export let description
+  export let featuredImage
+  export let url = `https://zackphilipps.dev/posts/${slug}`
 </script>
 
-<SEO {title} {slug} {description} />
+<SEO {title} {slug} {description} {featuredImage} {url} />
 <slot />
--- src/lib/components/SEO.svelte
+++ src/lib/components/SEO.svelte
@@ -4,7 +4,7 @@
   export let description: string
   export let slug: string
   export let featuredImage: string
-  export let url = `https://zackphilipps.dev/posts/${slug}`
+  export let url: string
 
   $pageMeta = { title, description, slug, featuredImage, url }
 </script>
@@ -18,11 +18,15 @@
   <meta property="og:type" content="article" />
   <meta property="og:title" content={title} />
   <meta property="og:description" content={description} />
-  <meta property="og:image" content={featuredImage ?? ''} />
+  {#if featuredImage}
+    <meta property="og:image" content={featuredImage} />
+  {/if}
   <meta property="og:url" content={url} />
 
   <!-- Twitter -->
   <meta name="twitter:title" content={title} />
   <meta name="twitter:description" content={description} />
-  <meta name="twitter:image" content={featuredImage} />
+  {#if featuredImage}
+    <meta name="twitter:image" content={featuredImage} />
+  {/if}
 </svelte:head>
